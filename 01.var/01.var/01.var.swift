6dc02b5b96fb3940a04b388d8508b28bd982bc46
gogleset <google1130@naver.com>
2022-01-02 11:24:02 +0900

    test

---
+++ 01.var/01.var/01.var.swift
@@ -0,0 +1,10 @@
+//
+//  01.var.swift
+//  01.var
+//
+//  Created by choijin on 2022/01/02.
+//
+
+import Foundation
+
+var a = 1;
---
+++ 01.var/01.var/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  01.var
+//
+//  Created by choijin on 2022/01/02.
+//
+
+import Foundation
+
+print("Hello, World!")
+
